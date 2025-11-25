acd739f7b4d90d85c4f42bdecbebb784941f9105
darkplayzmc2-svg <darkplayzmc2@gmail.com>
2025-11-25 20:46:24 +0530

    Update authMiddleware.js

--- flashpanel-backend/flashpanel-backend/authMiddleware.js
+++ flashpanel-backend/flashpanel-backend/authMiddleware.js
@@ -1,1 +1,10 @@
+module.exports = (req, res, next) => {
+    const key = req.headers["flash-key"];
+
+    if (!key || key !== "flashpanel_secret_123") {
+        return res.status(403).json({ message: "Unauthorized request" });
+    }
+
+    next();
+};
 
